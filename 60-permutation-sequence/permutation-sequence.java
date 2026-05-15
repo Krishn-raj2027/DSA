74fed944059b9b39fcd94c5d9c70cea26e211914
Krishn Raj <krishnraj8368@gmail.com>
2026-05-15 23:54:59 +0530

    Time: 1 ms (99.44%) | Memory: 42.9 MB (40.18%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 1; i<=n; i++) {
+            numbers.add(i);
+        }
+        int[] fact = new int[n + 1];
+        fact[0] = 1;
+        for (int i = 1; i<=n; i++) {
+            fact[i] = fact[i-1]*i;
+        }
+        k--;
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=n; i>=1; i--) {
+            int index = k/fact[i-1];
+            ans.append(numbers.get(index));
+            numbers.remove(index);
+            k = k % fact[i - 1];
+        }
+        return ans.toString();
+        
+    }
+}
